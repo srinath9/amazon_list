c2ff5637c21158e3e48a7276fc74bb364f803fdd
srinath9 <ksrianthchowdary9@gmail.com>
2015-05-19 23:00:38 +0530

    got all the html files

--- amazon_list.py
+++ amazon_list.py
@@ -12,27 +12,39 @@ def get_class_feature(z,class_name,fopen):
 	print "printing the data into files"
 	soup = BeautifulSoup(z.text)
 	fopen.write("giving the "+class_name)
-	for link in soup.findAll('a',{'class' : class_name}):
+	for link in soup.findAll('div',{'class' : class_name}):
+		print "data classs into the file"
+		print link.find('a')['href']
+		fopen.write(link.find('a')['href']+'\n')
+
+def get_span(z,class_name,fopen):
+	print "printing the data into files"
+	soup = BeautifulSoup(z.text)
+	fopen.write("giving the "+class_name)
+	for link in soup.findAll('span',{'class' : class_name}):
 		print "data classs into the file"
 		fopen.write(link.get('src')+'\n')
-	
+
+list_name = open("names.txt",'w')
 
 for link in soup.findAll('a',{'class':'nav_a'}):
 	filename =str(link.text)
-	filename = filename.replace('.','_') + '.txt'
+	filename = filename.replace('.','_').replace(" ","") + '.html'
 	print link.text
 	fopen = open(filename,'w')
 	href = link.get('href')
 	print  link.get('href')
+
 	try:
 		print "prin nwew"
 		
 		next_page = requests.get(href)
+		fopen.write(str(BeautifulSoup(next_page.text)))
 		print "Sdfsdfjksdjvndksnk sdjvsdvjs jsd djh"
-		get_class_feature(next_page,'fg-mobile-left',fopen)          
-		print "sdnfnooino"        #getting featues name
-		get_class_feature(next_page,'refinementLink',fopen)    
+		# get_class_feature(next_page,'localImage',fopen)          
+		# print "sdnfnooino"        #getting featues name
+		# get_span(next_page,'refinementLink',fopen)    
 	except :
 		print "some problem"
 		
-	         #gettting product name
+	         
